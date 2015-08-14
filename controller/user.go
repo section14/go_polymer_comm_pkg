389b0da3170c6766ae6aa365df99fa1cd8c8884c
section14 <henduism@gmail.com>
2015-08-13 22:56:15 -0400

    created controller package

---
+++ controller/user.go
@@ -0,0 +1,15 @@
+package controller
+
+import (
+    //"net/http"
+    //"log"
+    //"github.com/section14/go_polymer_comm_pkg/model"
+)
+
+type User struct {
+
+}
+
+func (u *User) TestHit() {
+
+}
