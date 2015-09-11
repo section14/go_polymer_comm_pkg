069db3a7cfbe8b141ec3f115f19bedb588071fef
section14 <henduism@gmail.com>
2015-09-10 22:48:21 -0400

    address queries

--- controller/address.go
+++ controller/address.go
@@ -9,7 +9,17 @@ import (
 )
 
 type Address struct {
-    UserId int64
+    UserId int64 `json:"userid"`
+    Street1 string `json:"street1"`
+    Street2 string `json:"street2"`
+    City string `json:"city"`
+    State string `json:"state"`
+    PostCode string `json:"postcode"`
+    Country string `json:"country"`
+}
+
+type AddressReturn struct {
+    Id int64 `json:"street1"`
     Street1 string `json:"street1"`
     Street2 string `json:"street2"`
     City string `json:"city"`
@@ -24,8 +34,6 @@ func (a *Address) CreateAddress(r *http.Request) (bool, error) {
     //grab json data from request
     decoder := json.NewDecoder(r.Body)
     err := decoder.Decode(&a)
-
-    log.Println(a)
 
     //populate model
     addressModel.Street1 = a.Street1
@@ -42,4 +50,69 @@ func (a *Address) CreateAddress(r *http.Request) (bool, error) {
     }
 
     return true, nil
+}
+
+func (a *Address) GetAllAddress(r *http.Request) ([]AddressReturn, error) {
+    addressModel := model.Address{}
+    addressModel.UserId = a.UserId
+    addressData, err := addressModel.GetAllAddress(r)
+
+    if err != nil {
+        return []AddressReturn{}, err
+    }
+
+    //return address data slices
+    results := make([]AddressReturn, 0, 10)
+
+    for _, r := range addressData {
+        y := AddressReturn {
+            Id: r.Id,
+            Street1: r.Street1,
+            Street2: r.Street2,
+            City: r.City,
+            State: r.State,
+            PostCode: r.PostCode,
+            Country: r.Country,
+        }
+
+        results = append(results, y)
+    }
+
+    /*
+    address := []AddressReturn {
+        Id: addressData.Id,
+        Street1: addressData.Street1,
+        Street2: addressData.Street2,
+        City: addressData.City,
+        State: addressData.State,
+        PostCode: addressData.PostCode,
+        Country: addressData.Country,
+    }
+    */
+
+    log.Println(results)
+
+    return results, nil
+}
+
+func (a *Address) GetAddress(r *http.Request) (AddressReturn, error) {
+    addressModel := model.Address{}
+
+    addressData,err := addressModel.GetAddress(r)
+
+    if err != nil {
+        return AddressReturn{}, err
+    }
+
+    address := AddressReturn {
+        Id: addressData.Id,
+        Street1: addressData.Street1,
+        Street2: addressData.Street2,
+        City: addressData.City,
+        State: addressData.State,
+        PostCode: addressData.PostCode,
+        Country: addressData.Country,
+    }
+
+    return address, nil
 }
--- model/address.go
+++ model/address.go
@@ -2,13 +2,24 @@ package model
 
 import (
     "net/http"
-    //"log"
+    "log"
     "appengine"
     "appengine/datastore"
 )
 
 type Address struct {
     UserId int64
+    Street1 string
+    Street2 string
+    City string
+    State string
+    PostCode string
+    Country string
+}
+
+type AddressReturn struct {
+    Key *datastore.Key
+    Id int64
     Street1 string
     Street2 string
     City string
@@ -32,4 +43,86 @@ func (a *Address) CreateAddress(r *http.Request) (error) {
     }
 
     return nil
+}
+
+func (a *Address) GetAllAddress(r *http.Request) ([]AddressReturn, error) {
+    //get context
+    c := appengine.NewContext(r)
+
+    //get key
+    //k := datastore.NewKey(c, "Address", "", a.UserId, nil)
+
+    //start query
+    q := datastore.NewQuery("Address").Filter("UserId=", a.UserId)
+
+    log.Println("model user id: ", a.UserId)
+
+    //populate address slices and get keys
+    var addresses []AddressReturn
+    keys, err := q.GetAll(c, &addresses)
+
+    if err != nil {
+        //handle error
+        return []AddressReturn{}, err
+    }
+
+    //return address data slices
+    results := make([]AddressReturn, 0, 10)
+
+    for i, r := range addresses {
+        k := keys[i]
+        y := AddressReturn {
+            Key: k,
+            Id: k.IntID(),
+            Street1: r.Street1,
+            Street2: r.Street2,
+            City: r.City,
+            State: r.State,
+            PostCode: r.PostCode,
+            Country: r.Country,
+        }
+
+        results = append(results, y)
+    }
+
+    return results, nil
+}
+
+func (a *Address) GetAddress(r *http.Request) (AddressReturn, error) {
+    //get context
+    c := appengine.NewContext(r)
+
+    //get key
+    k := datastore.NewKey(c, "Address", "", a.UserId, nil)
+
+    //start query
+    q := datastore.NewQuery("Address").Filter("__key__ =", k)
+
+    //populate address slices and get keys
+    var addresses []AddressReturn
+    keys, err := q.GetAll(c, &addresses)
+
+    if err != nil {
+        //handle error
+        return AddressReturn{}, err
+    }
+
+    //return address data
+    var result AddressReturn
+
+    for i, r := range addresses {
+        k := keys[i]
+        result = AddressReturn {
+            Key: k,
+            Id: k.IntID(),
+            Street1: r.Street1,
+            Street2: r.Street2,
+            City: r.City,
+            State: r.State,
+            PostCode: r.PostCode,
+            Country: r.Country,
+        }
+    }
+
+    return result, nil
 }
